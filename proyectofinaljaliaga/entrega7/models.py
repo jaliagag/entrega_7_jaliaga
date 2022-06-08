0ac76a4d65c576afa9186bdf36ae4c8658f60ec3
jaliaga_cenco <jose.aliaga@externos-ar.cencosud.com>
2022-06-08 01:21:08 -0300

    add display data to admin pannel

--- proyectofinaljaliaga/entrega7/models.py
+++ proyectofinaljaliaga/entrega7/models.py
@@ -4,6 +4,9 @@ class User(models.Model):
     user_name = models.CharField(max_length=40)
     passw = models.CharField(max_length=40)
     email = models.EmailField()
+    def __srt__(self) -> str:
+        return self.user_name+' '+str(self.passw)+' '+str(self.email)
+
 
 class Book(models.Model):
     title = models.CharField(max_length=40)
@@ -13,8 +16,14 @@ class Book(models.Model):
     isbn = models.IntegerField()
     date = models.DateField()
 
+    def __srt__(self) -> str:
+        return self.title+' '+str(self.description)+' '+str(self.genre)+' '+str(self.author)+' '+str(self.isbn)+' '+str(self.date)
+
 class Comment(models.Model):
     title= models.CharField(max_length=40)
     text = models.CharField(max_length=200)
     date = models.DateField()
+
+    def __srt__(self) -> str:
+        return self.title+' '+str(self.text)+' '+str(self.date)
 
